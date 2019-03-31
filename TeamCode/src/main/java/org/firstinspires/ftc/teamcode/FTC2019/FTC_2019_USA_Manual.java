95b1d8f886b94b9c76c38b6a6026ad2cd4f1582d
E311-29 <puichingrobotics@gmail.com>
2019-03-31 12:49:20 +0800

    Re-organised Manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2019/FTC_2019_USA_Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2019/FTC_2019_USA_Manual.java
@@ -5,14 +5,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 @TeleOp(name="FTC_2019_USA_Manual", group="FTC 2019")
-public class FTC_2019_USA_Manual extends OpMode{
+public class FTC_2019_USA_Manual extends OpMode {
 
     FTC_2019_USA_Init robot = new FTC_2019_USA_Init();
 
     double speedmodifier = 1;
 
+    public boolean Sweep = true;
+    public boolean Check = true;
     @Override
-    public void init(){
+    public void init() {
         robot.init(hardwareMap);
         robot.Lfront.setPower(0);
         robot.Rfront.setPower(0);
@@ -27,7 +29,7 @@ public class FTC_2019_USA_Manual extends OpMode{
     }
 
     @Override
-    public void start(){
+    public void start() {
 
     }
 
@@ -48,11 +50,11 @@ public class FTC_2019_USA_Manual extends OpMode{
         double dpadpower = .2;
         double dpadturningpower = .4;
 
-        /*if (gamepad1.right_bumper) {
-            speedmodifier = .5;
-        }
-        if (gamepad1.left_bumper) {
+        if (gamepad1.left_stick_button) {
             speedmodifier = 1;
+        }
+        if (gamepad1.right_stick_button) {
+            speedmodifier = .5;
         }
 
         if (gamepad1.dpad_up) {
@@ -63,100 +65,120 @@ public class FTC_2019_USA_Manual extends OpMode{
             leftsticky = -dpadpower;
         } else if (gamepad1.dpad_left) {
             leftstickx = -dpadturningpower;
-        } else {*/
-        leftstickx = gamepad1.left_stick_x;
-        leftsticky = -gamepad1.left_stick_y;
-        rightstickx = gamepad1.right_stick_x * speedmodifier;
-        //{
-        if (Math.abs(leftsticky) <= .15) {
-            leftsticky = 0;
-        }
-        wheelpower = Math.hypot(leftstickx, leftsticky);
-        stickangleradians = Math.atan2(leftsticky, leftstickx);
-
-        stickangleradians = stickangleradians - Math.PI / 4; //adjust by 45 degrees
-
-        rightX = rightstickx * .5;
-        leftfrontpower = wheelpower * Math.cos(stickangleradians) + rightX;
-        rightfrontpower = wheelpower * Math.sin(stickangleradians) - rightX;
-        leftrearpower = wheelpower * Math.sin(stickangleradians) + rightX;
-        rightrearpower = wheelpower * Math.cos(stickangleradians) - rightX;
-
-        robot.Lfront.setPower(leftfrontpower);
-        robot.Rfront.setPower(rightfrontpower);
-        robot.Lback.setPower(leftrearpower);
-        robot.Rback.setPower(rightrearpower);
-
-
-        //Control Latching
-        if (gamepad1.x){
-            robot.Latch.setPower(1);
-        }
-        else if (gamepad1.y){
-            robot.Latch.setPower(-1);
-        }else{
-            robot.Latch.setPower(0);
+        } else {
+            leftstickx = gamepad1.left_stick_x;
+            leftsticky = -gamepad1.left_stick_y;
+            rightstickx = gamepad1.right_stick_x * speedmodifier;
         }
 
-        //Control LS(s)
-        if (gamepad1.right_bumper){ //Down
-            robot.LS2.setPower(-1);
-        }
-        else if (gamepad1.left_bumper){
-            robot.LS2.setPower(1);
-        }else{
-            robot.LS2.setPower(0);
-        }
 
-        if (gamepad1.dpad_up){
-            robot.Hammer.setPosition(0.5);
-        }else if (gamepad1.dpad_left){
-            robot.Hammer.setPosition(0.2);
-        }else if (gamepad1.dpad_down){
-            robot.Hammer.setPosition(0.03);
-        }
+                if (Math.abs(leftsticky) <= .15) {
+                    leftsticky = 0;
+                }
+                wheelpower = Math.hypot(leftstickx, leftsticky);
+                stickangleradians = Math.atan2(leftsticky, leftstickx);
 
-        /*if (gamepad1.x){ //back
-            robot.MCR.setPosition(0.7);
-            robot.MCL.setPosition(0.3);
-        }
-        else if (gamepad1.y){
-            robot.MCR.setPosition(0.2);
-            robot.MCL.setPosition(0.8);
-        }*/
+                stickangleradians = stickangleradians - Math.PI / 4; //adjust by 45 degrees
 
-        if (gamepad2.b){
-            robot.MC.setPower(1);
-        }
-        else if (gamepad2.a){
-            robot.MC.setPower(0);
-        }
+                rightX = rightstickx * .5;
+                leftfrontpower = wheelpower * Math.cos(stickangleradians) + rightX;
+                rightfrontpower = wheelpower * Math.sin(stickangleradians) - rightX;
+                leftrearpower = wheelpower * Math.sin(stickangleradians) + rightX;
+                rightrearpower = wheelpower * Math.cos(stickangleradians) - rightX;
 
-        if (gamepad2.x){
-            robot.MCRotate.setPower(1);
-        }
-        else if (gamepad2.y){
-            robot.MCRotate.setPower(-1);
-        }
-        else {
-            robot.MCRotate.setPower(0);
-        }
+                robot.Lfront.setPower(leftfrontpower);
+                robot.Rfront.setPower(rightfrontpower);
+                robot.Lback.setPower(leftrearpower);
+                robot.Rback.setPower(rightrearpower);
 
-        if (gamepad2.left_bumper){
-            robot.LS1.setPower(1);
-        }
-        else if (gamepad2.right_bumper){
-            robot.LS1.setPower(-0.5);
-        }
-        else {
-            robot.LS1.setPower(0);
-        }
+                //Player 1
+                //Latching
+                if (gamepad1.left_bumper) {
+                    robot.Latch.setPower(1);
+                } else if (gamepad1.right_bumper) {
+                    robot.Latch.setPower(-1);
+                } else {
+                    robot.Latch.setPower(0);
+                }
 
-        //Player2
-        telemetry.update();
-    }
-    @Override
-    public void stop(){
+                //Mineral LS
+                if (gamepad1.left_trigger > 0.1) { //Down
+                    robot.LS2.setPower(-gamepad1.left_trigger);
+                } else if (gamepad1.right_trigger > 0.1) {
+                    robot.LS2.setPower(gamepad1.right_trigger);
+                } else {
+                    robot.LS2.setPower(0);
+                }
 
-    }
+                //Player 2
+                //Sorter Position
+                if (gamepad2.dpad_up) {
+                    robot.Hammer.setPosition(0.5);
+                } else if (gamepad2.dpad_left) {
+                    robot.Hammer.setPosition(0.2);
+                } else if (gamepad2.dpad_down) {
+                    robot.Hammer.setPosition(0.03);
+                }
+
+                //Sweeping
+                if (gamepad2.b && Sweep == true) {
+                    Sweep = false;
+                    if (Check) {
+                        robot.MC.setPower(1);
+                        Check = false;
+                    } else {
+                        robot.MC.setPower(0);
+                        Check = true;
+                    }
+                }
+                if(gamepad2.b == false && Sweep == false){
+                    Sweep = true;
+                }
+                if(gamepad2.a && Sweep == true){
+                    Sweep = false;
+                    if (Check) {
+                        robot.MC.setPower(-1);
+                        Check = false;
+                    } else {
+                        robot.MC.setPower(0);
+                        Check = true;
+                    }
+                }
+                if (gamepad2.a == false && Sweep == false){
+                    Sweep = true;
+                }
+
+                //MC Servo(s)
+                if (gamepad1.x){ //back
+                    robot.MCR.setPosition(0.7);
+                    robot.MCL.setPosition(0.3);
+                }
+                else if (gamepad1.y){
+                    robot.MCR.setPosition(0.2);
+                    robot.MCL.setPosition(0.8);
+                }
+                //MC Motor Rotation
+                if (gamepad2.left_trigger > 0.1) {
+                    robot.MCRotate.setPower(gamepad2.left_trigger);
+                } else if (gamepad2.right_trigger > 0.1) {
+                    robot.MCRotate.setPower(-gamepad2.right_trigger);
+                } else {
+                    robot.MCRotate.setPower(0);
+                }
+
+                //Sorter LS
+                if (gamepad2.left_bumper) {
+                    robot.LS1.setPower(1);
+                } else if (gamepad2.right_bumper) {
+                    robot.LS1.setPower(-0.5);
+                } else {
+                    robot.LS1.setPower(0);
+                }
+                telemetry.update();
+            }
+
+        @Override
+        public void stop () {
+
+        }
 }
