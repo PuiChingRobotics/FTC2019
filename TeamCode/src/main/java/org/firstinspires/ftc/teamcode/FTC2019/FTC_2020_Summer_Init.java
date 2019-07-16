511ea3efc689612ee10ad0ebde7d82f0399b77d4
wchunhei <wchunhei@gmail.com>
2019-07-16 12:43:25 +0800

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2019/FTC_2020_Summer_Init.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.FTC2019;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class FTC_2020_Summer_Init {
+    //Drive Motors
+    public DcMotor Lfront;
+    public DcMotor Rfront;
+    public DcMotor Lback;
+    public DcMotor Rback;
+
+    public HardwareMap _hw;
+
+    public void init(HardwareMap hw) {
+        _hw=hw;
+        Lfront = _hw.dcMotor.get("Lfront");
+        Rfront = _hw.dcMotor.get("Rfront");
+        Lback = _hw.dcMotor.get("Lback");
+        Rback = _hw.dcMotor.get("Rback");
+
+        Lfront.setDirection(DcMotorSimple.Direction.REVERSE);
+        Lback.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Rfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Rback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    public void runModeSet(String mode) {
+        if (mode == "position") {
+            Lfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Lback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Rfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Rback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if (mode == "reset") {
+            Lfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Lback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Rback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        } else if (mode == "tele") {
+            Lfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Lback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Rback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        } else if (mode == "encoder") {
+            Lfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Lback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Rback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+}
