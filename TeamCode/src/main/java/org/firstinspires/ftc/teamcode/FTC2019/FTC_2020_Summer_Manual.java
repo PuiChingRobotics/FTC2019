c3075bbadab0a23b61462fed7f2c823646a23276
wchunhei <wchunhei@gmail.com>
2019-07-16 12:55:14 +0800

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2019/FTC_2020_Summer_Manual.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.FTC2019;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="FTC_2020_Summer_Manual", group="FTC 2019")
+public class FTC_2020_Summer_Manual extends OpMode {
+
+    FTC_2020_Summer_Init robot = new FTC_2020_Summer_Init();
+
+    double speedmodifier = 1;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        robot.Lfront.setPower(0);
+        robot.Rfront.setPower(0);
+        robot.Lback.setPower(0);
+        robot.Rback.setPower(0);
+    }
+
+    @Override
+    public void loop() {
+        //drive
+        double leftstickx = 0;
+        double leftsticky = 0;
+        double rightstickx = 0;
+        double wheelpower;
+        double stickangleradians;
+        double rightX;
+        double leftfrontpower;
+        double rightfrontpower;
+        double leftrearpower;
+        double rightrearpower;
+        double dpadpower = .2;
+        double dpadturningpower = .4;
+
+        if (gamepad1.left_stick_button) {
+            speedmodifier = 1;
+        }
+        if (gamepad1.right_stick_button) {
+            speedmodifier = .5;
+        }
+
+        if (gamepad1.dpad_up) {
+            leftsticky = dpadpower;
+        } else if (gamepad1.dpad_right) {
+            leftstickx = dpadturningpower;
+        } else if (gamepad1.dpad_down) {
+            leftsticky = -dpadpower;
+        } else if (gamepad1.dpad_left) {
+            leftstickx = -dpadturningpower;
+        } else {
+            leftstickx = gamepad1.left_stick_x;
+            leftsticky = -gamepad1.left_stick_y;
+            rightstickx = gamepad1.right_stick_x * speedmodifier;
+        }
+
+
+        if (Math.abs(leftsticky) <= .15) {
+            leftsticky = 0;
+        }
+        wheelpower = Math.hypot(leftstickx, leftsticky);
+        stickangleradians = Math.atan2(leftsticky, leftstickx);
+
+        stickangleradians = stickangleradians - Math.PI / 4; //adjust by 45 degrees
+
+        rightX = rightstickx * .5;
+        leftfrontpower = wheelpower * Math.cos(stickangleradians) + rightX;
+        rightfrontpower = wheelpower * Math.sin(stickangleradians) - rightX;
+        leftrearpower = wheelpower * Math.sin(stickangleradians) + rightX;
+        rightrearpower = wheelpower * Math.cos(stickangleradians) - rightX;
+
+        robot.Lfront.setPower(leftfrontpower);
+        robot.Rfront.setPower(rightfrontpower);
+        robot.Lback.setPower(leftrearpower);
+        robot.Rback.setPower(rightrearpower);
+    }
+}
