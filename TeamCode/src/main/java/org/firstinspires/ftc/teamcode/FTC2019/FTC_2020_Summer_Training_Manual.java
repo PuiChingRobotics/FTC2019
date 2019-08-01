439ffd07fd7c0287ab68d0015dd18666dea4ef05
E311-29 <puichingrobotics@gmail.com>
2019-08-01 15:05:07 +0800

    Fixed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2019/FTC_2020_Summer_Training_Manual.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.FTC2019;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="FTC_2020_Summer_Training_Manual", group="FTC 2019")
+public class FTC_2020_Summer_Training_Manual extends OpMode {
+
+    FTC_2020_Summer_Training_Init robot = new FTC_2020_Summer_Training_Init();
+
+    double speedmodifier = 1;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        robot.Lfront.setPower(0);
+        robot.Rfront.setPower(0);
+        robot.Lback.setPower(0);
+        robot.Rback.setPower(0);
+    }
+    public double RoundDownDp(double value, double place){
+        double result = value / place;
+        result = Math.floor(result)*place;
+        return result;
+    }
+    @Override
+    public void loop() {
+        /*
+        //drive
+        double leftstickx = 0;
+        double leftsticky = 0;
+        double rightstickx = 0;
+        double wheelpower;
+        double stickangleradians;
+        double rightX;
+        double leftfrontpower;
+        double rightfrontpower;
+        double leftrearpower;
+        double rightrearpower;
+        double dpadpower = .2;
+        double dpadturningpower = .4;
+
+        if (gamepad1.left_stick_button) {
+            speedmodifier = 1;
+        }
+        if (gamepad1.right_stick_button) {
+            speedmodifier = .5;
+        }
+
+        if (gamepad1.dpad_up) {
+            leftsticky = dpadpower;
+        } else if (gamepad1.dpad_right) {
+            leftstickx = dpadturningpower;
+        } else if (gamepad1.dpad_down) {
+            leftsticky = -dpadpower;
+        } else if (gamepad1.dpad_left) {
+            leftstickx = -dpadturningpower;
+        } else {
+            leftstickx = gamepad1.left_stick_x;
+            leftsticky = -gamepad1.left_stick_y;
+            rightstickx = gamepad1.right_stick_x * speedmodifier;
+        }
+
+
+        if (Math.abs(leftsticky) <= .15) {
+            leftsticky = 0;
+        }
+        wheelpower = Math.hypot(leftstickx, leftsticky);
+        stickangleradians = Math.atan2(leftsticky, leftstickx);
+
+        stickangleradians = stickangleradians - Math.PI / 4; //adjust by 45 degrees
+
+        rightX = rightstickx * .5;
+        leftfrontpower = wheelpower * Math.cos(stickangleradians) + rightX;
+        rightfrontpower = wheelpower * Math.sin(stickangleradians) - rightX;
+        leftrearpower = wheelpower * Math.sin(stickangleradians) + rightX;
+        rightrearpower = wheelpower * Math.cos(stickangleradians) - rightX;
+
+        robot.Lfront.setPower(leftfrontpower);
+        robot.Rfront.setPower(rightfrontpower);
+        robot.Lback.setPower(leftrearpower);
+        robot.Rback.setPower(rightrearpower);
+        */
+        double leftStickX = gamepad1.left_stick_x;
+        double leftStickY = -gamepad1.left_stick_y;
+        double rightStickX = gamepad1.right_stick_x;
+        double rightStickY = -gamepad1.right_stick_y;
+        double g2leftStickX = gamepad2.left_stick_x;
+        double g2leftStickY = -gamepad2.left_stick_y;
+        double g2rightStickX = gamepad2.right_stick_x;
+        double g2rightStickY = -gamepad2.right_stick_y;
+
+        double speed_new = 1-0.7;
+        double speed_old = 0.7;
+
+        telemetry.addData("Running", "Robot 1");
+
+        //player1
+
+        //drive
+        robot.Lfrontforward = RoundDownDp(robot.Lfrontforward*speed_old+leftStickY*speed_new, 0.001);
+        robot.Rfrontforward = RoundDownDp(robot.Rfrontforward*speed_old+leftStickY*speed_new, 0.001);
+        robot.Lbackforward = RoundDownDp(robot.Lbackforward*speed_old+leftStickY*speed_new, 0.001);
+        robot.Rbackforward = RoundDownDp(robot.Rbackforward*speed_old+leftStickY*speed_new, 0.001);
+
+        robot.Lfronttmp = RoundDownDp(leftStickY*1+rightStickX*0.7+leftStickX*0.7, 0.001);
+        robot.Lbacktmp = RoundDownDp(leftStickY*1+rightStickX*0.7-leftStickX*0.7, 0.001);
+        robot.Rfronttmp = RoundDownDp(leftStickY*1-rightStickX*0.7-leftStickX*0.7, 0.001);
+        robot.Rbacktmp = RoundDownDp(leftStickY*1-rightStickX*0.7+leftStickX*0.7, 0.001);
+
+        robot.Lfront.setPower(robot.Lfronttmp);
+        robot.Lback.setPower(robot.Lbacktmp);
+        robot.Rfront.setPower(robot.Rfronttmp);
+        robot.Rback.setPower(robot.Rbacktmp);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC2020/FTC_2020_summertesting_init.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.FTC2020;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+
+public class FTC_2020_summertesting_init {
+    //Dirve Motors
+    public DcMotor Lfront;
+    public DcMotor Rfront;
+    public DcMotor Lback;
+    public DcMotor Rback;
+    //LS no.1
+   /* public DcMotor LS1;
+    //Latching
+    public DcMotor Latch;
+    //MCRotate
+    public DcMotor MCRotate;
+    //LS no.2
+    public DcMotor LS2;
+
+    //MC
+    public CRServo MC;
+    public Servo MCL;
+    public Servo MCR;
+    //Drive
+    public Servo Hammer;*/
+    public double Lfronttmp = 0;
+    public double Lbacktmp = 0;
+    public double Rfronttmp = 0;
+    public double Rbacktmp = 0;
+
+    public double Lfrontforward = 0;
+    public double Lbackforward = 0;
+    public double Rfrontforward = 0;
+    public double Rbackforward = 0;
+
+
+    public double inc = 2.54;
+    //cam
+   // public WebcamName Webcam;
+    //imu
+    BNO055IMU imu;
+
+    public int Latch_Limit = -10500;
+
+    public HardwareMap _hw;
+
+    public void init(HardwareMap hw) {
+        _hw = hw;
+
+        Lfront = _hw.dcMotor.get("Lfront");
+        Rfront = _hw.dcMotor.get("Rfront");
+        Lback = _hw.dcMotor.get("Lback");
+        Rback = _hw.dcMotor.get("Rback");
+
+       /* MCRotate = _hw.dcMotor.get("MCRotate");
+
+        LS2 = _hw.dcMotor.get("LS2");
+
+        Hammer = _hw.servo.get("Hammer");
+
+        MCL = _hw.servo.get("MCL");
+        MCR = _hw.servo.get("MCR");
+
+        MC = _hw.crservo.get("MC");
+
+
+        Webcam = _hw.get(WebcamName.class, "Webcam");
+
+        imu = _hw.get(BNO055IMU.class, "imu");
+
+        Latch = _hw.dcMotor.get("Latch");
+
+        LS1 = _hw.dcMotor.get("LS1");*/
+
+        Lfront.setDirection(DcMotorSimple.Direction.REVERSE);
+        Lback.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Rfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Rback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+       /* LS1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LS2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        MCRotate.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
+    }
+
+    public void runModeSet(String mode) {
+        if (mode == "position") {
+            Lfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Lback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Rfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            Rback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if (mode == "reset") {
+            Lfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Lback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Rback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        } else if (mode == "tele") {
+            Lfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Lback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            Rback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        } else if (mode == "encoder") {
+            Lfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Lback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Rfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Rback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+}
+
